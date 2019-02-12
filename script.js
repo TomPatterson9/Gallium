f2f3627b3e2410ff6b3d337742e6286dc9beaef5
TomPatterson9 <35941859+TomPatterson9@users.noreply.github.com>
2019-02-12 02:35:49 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const scroller = new SweetScroll({             
+  	horizontal: true,              // Enable the horizontal scroll
+
+  });
+}, false);
+    var sweetScroll = new SweetScroll({
+      horizontal: true,
+    }, "#wrapper");
